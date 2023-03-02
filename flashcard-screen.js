908127dc358f1031b2a264df45cf39e2cc0315da
1n-hasee <2143572870@qq.com>
2023-03-02 19:45:56 +0800

    finish

--- flashcard-screen.js
+++ flashcard-screen.js
@@ -1,11 +1,3 @@
-// TODO(you): Modify the class in whatever ways necessary to implement
-// the flashcard app behavior.
-//
-// You may need to do things such as:
-// - Changing the constructor parameters
-// - Rewriting some of the existing methods, such as changing code in `show()`
-// - Adding methods
-// - Adding additional fields
 
 class FlashcardScreen {
   constructor(containerElement) {
--- flashcard.js
+++ flashcard.js
@@ -1,10 +1,3 @@
-// TODO(you): Modify the class in whatever ways necessary to implement
-// the flashcard app behavior.
-//
-// You may need to do things such as:
-// - Changing the constructor parameters
-// - Adding methods
-// - Adding additional fields
 
 class Flashcard {
   constructor(containerElement, frontText, backText) {
@@ -18,19 +11,7 @@ class Flashcard {
     this.flashcardElement.addEventListener('pointerup', this._flipCard);
   }
 
-  // Creates the DOM object representing a flashcard with the given
-  // |frontText| and |backText| strings to display on the front and
-  // back of the card. Returns a reference to root of this DOM
-  // snippet. Does not attach this to the page.
-  //
-  // More specifically, this creates the following HTML snippet in JS
-  // as a DOM object:
-  // <div class="flas hcard-box show-word">
-  //   <div class="flashcard word">frontText</div>
-  //   <div class="flashcard definition">backText</div>
-  // </div>
-  // and returns a reference to the root of that snippet, i.e. the
-  // <div class="flashcard-box">
+ 
   _createFlashcardDOM(frontText, backText) {
     const cardContainer = document.createElement('div');
     cardContainer.classList.add('flashcard-box');
@@ -54,7 +35,6 @@ class Flashcard {
   }
 
   _flipCard(event) {
-    //切换
     this.flashcardElement.classList.toggle('show-word');
     this.wordshow = !this.wordshow;
   }
